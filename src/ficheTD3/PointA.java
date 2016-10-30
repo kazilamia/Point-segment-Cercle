aea40e6adda554ae877daed666d2ee125c5d0161
MF1996 <makam-elchahid@outlook.fr>
2016-10-30 21:01:58 +0000

    Update PointA.java

--- src/ficheTD3/PointA.java
+++ src/ficheTD3/PointA.java
@@ -5,18 +5,19 @@ public class PointA extends Point{
 
  public PointA (double x, double y)
     {
-        super (x,y);
+        super (x,y); //appelle  constructeur class mère(Point) avec deux  paramètre x et y
     }
     
     public PointA (double x){
-        super (x);
+        super (x); // appelle  constructeur class mère(Point) avec un seul paramètre x
     }
     
     public PointA (){
-        super ();
+           // appelle impmecite du constructeur par defaut de la class mère(Point) 
     }
     
     @Override
+//polymorphisme 
 	public String toString() {
 		return "(" + getX() + ", " + getY() + ")";
 	}
